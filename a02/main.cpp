51084596f6ff3c01ccbfc76f1bb35e22249797c3
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-11-25 00:09:56 +0900

    Add a02

---
+++ a02/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, x;
+  cin >> n >> x;
+  for (int i = 0; i < n; i++) {
+    int a;
+    cin >> a;
+    if (a == x) {
+      cout << "Yes\n";
+      return 0;
+    }
+  }
+
+  cout << "No\n";
+  return 0;
+}
