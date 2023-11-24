bc9597ec8b3dabbd09173057eb779c93a1d5c791
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-11-25 00:26:22 +0900

    Add b02

---
+++ b02/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  for (int i = a; i <= b; i++) {
+    if (100 % i == 0) {
+      cout << "Yes\n";
+      return 0;
+    }
+  }
+  cout << "No\n";
+  return 0;
+}
